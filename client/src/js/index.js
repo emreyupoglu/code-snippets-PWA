d671624c6b5153978d364a2dbe88fea74db2ef48
emronelli <emronelli@emrons-MacBook-Air.local>
2023-02-09 19:05:02 -0500

    filling TODOs

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -31,3 +31,13 @@ if ('serviceWorker' in navigator) {
 } else {
   console.error('Service workers are not supported in this browser.');
 }
+
+//added hot module reloading(HMR)
+
+if (module.hot) {
+  module.hot.accept((err) => {
+    if (err) {
+      console.error('Cannot apply HMR update.', err);
+    }
+  });
+}
